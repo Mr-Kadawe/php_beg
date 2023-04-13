ddadeb4def913a73dbffbfbc0756690a6ef53983
Abuukar saalah <mrkadawe@gmail.com>
2023-04-12 23:09:21 -0700

    update 2

--- index.php
+++ index.php
@@ -6,10 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Course</title>
 </head>
-<body>
+<body>    
     <h1>Welcome Php Courses </h1>
-    <?php 
-        echo  "Hello, world";
+    <?php
+
+       echo  "Hello, world";
 
         echo  "<p> <b> Hello Word </b>, Welcome php script</p>";
 
@@ -22,9 +23,10 @@
         echo "<b>I'm Here ...</b>";
 
 
-        // php comments (// /* */ )
+        // php comments (//,  /* */ , #)
         
         // php Variable(All variables in PHP start with a $ sign symbol)
+        echo  "<br>";
         echo  "<br>";
         $txt = "Hello World";
         // echo  ($txt);
@@ -50,24 +52,109 @@
         echo "<br>";
         var_dump($n);
 
+     
         echo "<hr>";
 
-        // concatenation operator (.) 
+        // // concatenation operator (.) 
 
         $txt1 = "HTML, CSS, JavaScript";
-        $txt2 = " And PHP Courses";
+        $txt2 = "And PHP Courses";
         
-        echo  $txt1 . $txt2;
+        echo  $txt1 . " " . $txt2;
 
         echo  "<br><BR>";
-        $txt1 .= $txt2;
+        $txt1 .= " ".$txt2;
         echo  $txt1;
 
+        // 07/04/2023
+        echo "<br>"; 
+
+        echo "$num / $n  =". $num/$n;
+            echo "<br>";
+        echo $num / $n;
+
+        // Date Function
+        echo "<br>";
+        $date = date('D');
+        $month = date('F');
+        $year = date('Y');
+        // echo $date;
+        // echo "/".$month;
+        // echo "/".$year;
+
+        echo " <br> DATE: ". $date .'/'.$month."/".$year;
+        
+        $d_m_y =  date('d/m/Y');
+        echo "<br> Date: ".$d_m_y;
+
+        echo "<br> Date And Time : " . date('d/m/Y h:i:s A');
+
+        // conditional ststement
+
+        echo "<h1> conditional ststement </h1>";
+        $date  = date('D');
+        if($date == 'fri'){
+            echo "Nasiino Wacan Mudane";
+        }
+       
+        // if($date ){
+        //     echo  "<br> Find This Variable";
+        // }
+        
+        if($date == 'Fri'){
+            echo "<br> Nasiino Wacan Mudane/Marwo";
+        }else{
+            echo "<br>Not Found Result Please Check Your Code ";
+        }
+
+        // != IS Not Equal 
+        if($date != 'Sat'){
+            echo "<br> $date is Not Equal Sat";
+        }else{
+            echo  "<br> $date Is equal to Sat";
+        }
+        // <> IS Not Equal 
+
+        if($date <> 'Sun'){
+            echo "<br> $date Is not equal Sun";
+        }else{
+            echo "<br> $date Is Equal to Sun";
+        }
 
 
-            
+        echo "<h2> Calculate AVerage</h2>";
+        $avg = 95.1;
+
+        if($avg > 100 && $avg < 0){
+            echo  "Invalid Grade ";
+        }elseif ($avg <= 100 && $avg > 95) {
+           echo "Your Grade A+";
+        }else if($avg <= 95 && $avg > 85){
+            echo "Your Grade A-";
+        }else  if($avg < 50){
+            echo "Failed";        
+        }else{
+            echo "Not Found ";
+        }
 
 
-    ?>
+
+
+
+
+
+
+
+
+
+
+
+        
+        // echo "<h1> Tijaabo</h1>";            
+        // echo ("<h1> Tijaabo One</h1>");            
+
+
+ ?>
+
 </body>
 </html>
